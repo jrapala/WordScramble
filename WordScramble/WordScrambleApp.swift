aa06d3dc5a91ab0777869af46c2b214fdd8085fa
Juliette Rapala <jrapala@gmail.com>
2020-10-03 17:07:03 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Juliette Rapala on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
