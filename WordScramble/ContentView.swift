a242f072f95a925b10325f847259105e8ab3aba9
Juliette Rapala <jrapala@gmail.com>
2020-10-04 13:51:28 -0500

    feat: add game methods

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -47,6 +47,31 @@ struct ContentView: View {
         newWord = ""
     }
     
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
     func startGame() {
         // 1. Find the URL for the file in our app bundle
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
