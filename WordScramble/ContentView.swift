47b1ee5b1764622fae20c9df41bb226e8c9b3914
Juliette Rapala <jrapala@gmail.com>
2020-10-04 13:37:57 -0500

    feat: add and display list of entered words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+                
+                List(usedWords, id: \.self) {
+                    // Add icon with word length in circle (1-50 is available)
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        // 1. Lowercase the newWord and trim
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // 2. Check if there is at least one character
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // 3. Insert the word at beginning of usedWords
+        usedWords.insert(answer, at: 0)
+        
+        // 4. Reset newWord
+        newWord = ""
     }
 }
 
